bc7e010ca0fc0833f7bd2603a5c691eaa7bb66f3
azamat <bravlstarstop05@mail.ru>
2025-03-20 10:37:49 +0500

    create CRUD

--- src/main/java/com/example/springapplication/entity/Category.java
+++ src/main/java/com/example/springapplication/entity/Category.java
@@ -1,7 +1,11 @@
 package com.example.springapplication.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "category")
 public class Category {
--- src/main/java/com/example/springapplication/entity/User.java
+++ src/main/java/com/example/springapplication/entity/User.java
@@ -2,7 +2,9 @@ package com.example.springapplication.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Entity
 @Table(name = "users")
